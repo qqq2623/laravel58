8ca2d37cf5dc5068fc4915429ab322dffa487297
zhangyu <zhangyu@xiongying.com>
2019-07-19 14:38:07 +0800

    ES集群没成功

--- upstream.lua
+++ upstream.lua
@@ -1,1 +1,7 @@
 local uri_args=ngx.req.get_uri_args() -- 获取uri的参数
+local id = uri_args['id']
+local server = { '119.3.109.0:9800', '119.3.109.0:9801' }
+local index = ngx.crc32_long(id) % table.getn(server)
+
+uri = "http://" .. server[index]
+require();
